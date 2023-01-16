ca6e6641acef9b6f5ee3d60e145a35e283cdb711
emirhan.oruc <emirhan.oruc@hotmail.com>
2023-01-16 20:57:39 +0300

    Added Spring Data Repositories

---
+++ spring5webapp/src/main/java/eoruc/spring/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,8 @@
+package eoruc.spring.spring5webapp.repositories;
+
+import eoruc.spring.spring5webapp.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+
+}
